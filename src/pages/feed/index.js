04ea9a27315fe5bd3fa274f1b4fa9a3180da7cf4
Talita-8 <tlthmf1@outlook.com>
2021-04-04 18:48:33 -0300

    Header Shadow

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,10 +1,10 @@
 
 export const Feed = () => {
-   
-    // const rootElement = document.createElement('div');
-    // rootElement.classList.add('')
-    // rootElement.innerHTML = 
-   
+  const rootElement = document.createElement("div");
+  rootElement.classList.add("feed-container")
+  rootElement.innerHTML = `
+   <p> oi, feed :D </p>
+    `
   return rootElement;
 };
 
